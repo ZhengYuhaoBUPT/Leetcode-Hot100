f2129bfa3291cf45d6ca638926101a1c94cd1dae
郑雨昊 <1067992289@qq.com>
2025-07-17 23:43:04 +0800

    Create group-anagrams.py

---
+++ Hash/group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        mp = collections.defaultdict(list)
+
+        for st in strs:
+            counts = [0] * 26
+            for ch in st:
+                counts[ord(ch) - ord("a")] += 1
+            mp[tuple(counts)].append(st)
+
+        return list(mp.values())
