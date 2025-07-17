95d34888b7b12c59f81d1b283997ab6ad28b16a0
郑雨昊 <1067992289@qq.com>
2025-07-18 01:06:59 +0800

    Update container-with-most-water.py

--- Double-Pointer/container-with-most-water.py
+++ Double-Pointer/container-with-most-water.py
@@ -1,1 +1,19 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
+        left, right = 0, len(height) - 1
+        max_volume = 0
 
+        while left < right:
+            high = min(height[left], height[right])
+            max_volume = max(max_volume, high * (right - left))
+            if high == height[left]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_volume
