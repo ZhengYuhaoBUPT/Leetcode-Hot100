06657b4634594ea6e6ff332c6f7b0e120157af79
郑雨昊 <1067992289@qq.com>
2025-07-21 21:53:47 +0800

    Create maximum-depth-of-binary-tree.py

---
+++ Binary-Tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height, right_height) + 1
