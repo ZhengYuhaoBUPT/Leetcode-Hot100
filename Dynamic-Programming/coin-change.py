dcf53891bd4cdf406b0740b7e539cd220cfd1fe5
郑雨昊 <1067992289@qq.com>
2025-07-22 02:01:56 +0800

    Create coin-change.py

---
+++ Dynamic-Programming/coin-change.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+
+        if amount == 0:
+            return 0
+        
+        dp = [0] + [1e9] * (amount)
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return dp[-1] if dp[-1] != 1e9 else -1
