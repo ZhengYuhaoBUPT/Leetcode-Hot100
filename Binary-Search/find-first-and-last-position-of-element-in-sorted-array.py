c50d6fb2d692895069a4f3e3bb99eabf14569c92
郑雨昊 <1067992289@qq.com>
2025-07-22 12:00:01 +0800

    Create find-first-and-last-position-of-element-in-sorted-array.py

---
+++ Binary-Search/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lower_band(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] >= target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        start = self.lower_band(nums, target)
+        if start == len(nums) or nums[start] != target:
+            return [-1, -1]  # nums 中没有 target
+        # 如果 start 存在，那么 end 必定存在
+        end = self.lower_band(nums, target + 1) - 1
+        return [start, end]
