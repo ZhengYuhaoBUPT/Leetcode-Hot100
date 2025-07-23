5b33920ce1eb86fc2b2cc0738ee1854401728859
郑雨昊 <1067992289@qq.com>
2025-07-24 03:07:59 +0800

    Create invert-binary-tree.py

---
+++ Binary-Tree/invert-binary-tree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+        root.left, root.right = right, left
+
+        return root
