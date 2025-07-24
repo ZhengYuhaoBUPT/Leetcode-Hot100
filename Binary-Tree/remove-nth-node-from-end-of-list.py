7c707d0d1775bf0667ce17af0103dfb961c6c39d
郑雨昊 <1067992289@qq.com>
2025-07-24 17:45:01 +0800

    Create remove-nth-node-from-end-of-list.py

---
+++ Binary-Tree/remove-nth-node-from-end-of-list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        
+        left = right = dummy = ListNode(next=head)
+        for _ in range(n):
+            right = right.next
+        while right.next:
+            left = left.next
+            right = right.next
+        left.next = left.next.next
+
+        return dummy.next
