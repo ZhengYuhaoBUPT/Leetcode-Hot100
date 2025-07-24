261404cdadc4e405d7119bbc09862800b26b89d6
郑雨昊 <1067992289@qq.com>
2025-07-24 17:52:59 +0800

    Create swap-nodes-in-pairs.py

---
+++ Binary-Tree/swap-nodes-in-pairs.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+        if not head or not head.next:
+            return head
+        
+        newHead = head.next
+        head.next = self.swapPairs(head.next.next)
+        newHead.next = head
+
+        return newHead
