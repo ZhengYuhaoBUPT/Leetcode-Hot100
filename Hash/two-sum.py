1de7bdaf3faab9d81e93dc16adbf9e504b9be22f
郑雨昊 <1067992289@qq.com>
2025-08-25 16:05:30 +0800

    Update two-sum.py

--- Hash/two-sum.py
+++ Hash/two-sum.py
@@ -6,10 +6,14 @@ class Solution(object):
         :rtype: List[int]
         """
 
+        # make a hashtable
         hashtable = dict()
-        for i, num in enumerate(nums):
-            if target - num in hashtable:
-                return [hashtable[target - num], i]
+
+        # do the loop, for every number in nums,
+        # find if the target - number in hashtable
+        # if exist, return [hashtable[target-number], nums[i]]
+        # else, add num
+        for i in range(len(nums)):
+            if target - nums[i] in hashtable:
+                return [hashtable[target-nums[i]], i]
             hashtable[nums[i]] = i
-        
-        return []
