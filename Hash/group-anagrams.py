2abc3335c03d2d0026d6b3f9ad8442ddf2a89435
郑雨昊 <1067992289@qq.com>
2025-08-25 16:15:46 +0800

    Update group-anagrams.py

--- Hash/group-anagrams.py
+++ Hash/group-anagrams.py
@@ -5,12 +5,17 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         
-        mp = collections.defaultdict(list)
+        # make hashtable
+        hashtable = collections.defaultdict(list)
 
-        for st in strs:
-            counts = [0] * 26
-            for ch in st:
-                counts[ord(ch) - ord("a")] += 1
-            mp[tuple(counts)].append(st)
+        # for every str in strs, we need to add it in to hashtable
+        # but in order
+        for mystr in strs:
+            # for mystr, get the sorted order as the key
+            # we need to notice that in this mystr is not changed
+            mykey = "".join(sorted(mystr))
 
-        return list(mp.values())
+            # use the original mystr as the value
+            hashtable[mykey].append(mystr)
+
+        return list(hashtable.values())
