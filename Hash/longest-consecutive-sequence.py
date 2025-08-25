dddc8ecfa055c6f449d7380281720f02b973f232
郑雨昊 <1067992289@qq.com>
2025-08-25 16:27:02 +0800

    Update longest-consecutive-sequence.py

--- Hash/longest-consecutive-sequence.py
+++ Hash/longest-consecutive-sequence.py
@@ -5,18 +5,27 @@ class Solution(object):
         :rtype: int
         """
 
+        # record the longest number
         longest = 0
-        num_set = set(nums)
 
-        for num in num_set:
-            if num - 1 not in num_set:
-                current_num = num
-                current_steak = 1
+        # transform the nums into set
+        nums_set = set(nums)
 
-                while current_num + 1 in num_set:
-                    current_num += 1
-                    current_steak += 1
+        # do the loop to try all num in nums, find if num - 1 exist
+        for num in nums_set:
+            # if we try every number may beyond the time
+            # then, we just skip some
+            # only if num + 1 not in set, we do
+            # else the length must be shorter
+            if num + 1 not in nums_set:
+                # tmp for length of the num at this time
+                tmp = 1
+                # check nums - 1, use while for consecutive number before
+                while num - 1 in nums_set:
+                    tmp += 1
+                    num -= 1
 
-                longest = max(longest, current_steak)
-
+                # we need to choose the maximum length
+                longest = max(longest, tmp)
+        
         return longest
