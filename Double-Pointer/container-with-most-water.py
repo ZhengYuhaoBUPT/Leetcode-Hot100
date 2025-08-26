c89033c246be598b066efe6fac350f2012c6e290
郑雨昊 <1067992289@qq.com>
2025-08-26 18:32:02 +0800

    Update container-with-most-water.py

--- Double-Pointer/container-with-most-water.py
+++ Double-Pointer/container-with-most-water.py
@@ -4,16 +4,29 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        
-        left, right = 0, len(height) - 1
-        max_volume = 0
 
+        # first we need to have the length for loop
+        n = len(height)
+        
+        # two pointers from left and right
+        left, right = 0, n - 1
+
+        # we need a max_volumn
+        max_volumn = 0
+
+        # do the loop to find the max
         while left < right:
-            high = min(height[left], height[right])
-            max_volume = max(max_volume, high * (right - left))
-            if high == height[left]:
+            # we need to get the height of the container
+            # the height is the minimum number of the left and right
+            height_volumn = min(height[left], height[right])
+            # get the volumn and compare
+            max_volumn = max(max_volumn, height_volumn * (right - left))
+
+            # after that, we need to change left or right
+            if height_volumn == height[left]:
+                # change the left to get a bigger height
                 left += 1
             else:
                 right -= 1
-
-        return max_volume
+        
+        return max_volumn
