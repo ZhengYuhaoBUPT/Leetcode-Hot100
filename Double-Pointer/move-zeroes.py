ebe63c61e8d0f850d76008ae0deef2368f3a0c3c
郑雨昊 <1067992289@qq.com>
2025-08-26 18:21:47 +0800

    Update move-zeroes.py

--- Double-Pointer/move-zeroes.py
+++ Double-Pointer/move-zeroes.py
@@ -5,13 +5,30 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
+        # first get the length of the nums
         n = len(nums)
+
+        # use two pointers
+        # the left and right both start at index 0
+        # if num is not 0, both the pinter go and left = num
+        # else num is 0, the right pointer go, left do not go, zeronums+1
         left = right = 0
 
+        # the number of zero
+        zero_number = 0
+
+        # two loop, first find 0, second revise last few nums to zero
+        # do the first loop
         while right < n:
-            if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
+            if nums[right] == 0:
+                zero_number += 1
+            else:
+                nums[left] = nums[right]
                 left += 1
             right += 1
 
-        return nums
+        # do the second loop
+        while zero_number > 0:
+            nums[n - zero_number] = 0
+            zero_number -= 1
+    
