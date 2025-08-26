5996712fda2bb17f0441a30a63347b99ae857cff
郑雨昊 <1067992289@qq.com>
2025-08-27 06:09:08 +0800

    Update 3sum.py

--- Double-Pointer/3sum.py
+++ Double-Pointer/3sum.py
@@ -5,29 +5,35 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
+        # first we need to sort the nums
         nums.sort()
-        results = list()
+
+        # the length
         n = len(nums)
 
+        # the answer
+        answer = []
+
+        # then, we need to do the loop from the first
         for first in range(n):
-            if first > 0 and nums[first] == nums[first - 1]:
+            # we do not need the same first
+            if first > 0 and nums[first] == nums[first-1]:
                 continue
-            
+            # for every first number, find the second and the third
             third = n - 1
-            target = - nums[first]
-
-            # two number
+            # do the loop for second
             for second in range(first + 1, third):
-                if second > first + 1 and nums[second] == nums[second - 1]:
+                # cancel the same number
+                if second > first + 1 and nums[second] == nums[second-1]:
                     continue
-
-                while second < third and nums[second] + nums[third] > target:
+                # the answer is bigger than 0
+                while second < third and nums[second] + nums[third] > -nums[first]:
+                    # get smaller
                     third -= 1
-                
+                # second and third can not be the same
                 if second == third:
                     break
-                    
-                if nums[second] + nums[third] == target:
-                    results.append([nums[first], nums[second], nums[third]])
-
-        return results
+                if nums[second] + nums[third] == -nums[first]:
+                    answer.append([nums[first], nums[second], nums[third]])
+        
+        return answer
