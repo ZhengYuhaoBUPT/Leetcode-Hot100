27efe0f3a17aa70d8a4343b5ecd471718d3d6e2f
郑雨昊 <1067992289@qq.com>
2025-08-28 20:48:08 +0800

    Update sliding-window-maximum.py

--- Substring/sliding-window-maximum.py
+++ Substring/sliding-window-maximum.py
@@ -5,23 +5,36 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        
+
+        # it's a good idea to use deque
+        # initialize the length
         n = len(nums)
+        # initialize the list
         q = collections.deque()
 
+        # do the loop for k
         for i in range(k):
+            # we use q to record the index
+            # we make the max on the top
             while q and nums[i] >= nums[q[-1]]:
+                # pop the small one
                 q.pop()
+            # append the number
             q.append(i)
 
+        # use a list to record ans
         ans = [nums[q[0]]]
+        # do the loop for other
         for i in range(k,n):
-            while q and nums[i] >= nums[q[-1]]:
-                q.pop()
-            q.append(i)
-
+            # we need to pop the biggest if exit the window
             while q[0] <= i - k:
                 q.popleft()
+            # if bigger, pop the later
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+            
+            # add the ans
             ans.append(nums[q[0]])
 
         return ans
