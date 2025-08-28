4c5033b4ac6a1af075ad8bf967f8e344d8a5785c
郑雨昊 <1067992289@qq.com>
2025-08-28 21:23:24 +0800

    Update minimum-window-substring.py

--- Substring/minimum-window-substring.py
+++ Substring/minimum-window-substring.py
@@ -6,31 +6,38 @@ class Solution(object):
         :rtype: str
         """
         
-        if len(s) < len(t):
-            return ""
-        
-        hs, ht = defaultdict(int), defaultdict(int)
-        for char in t:
-            ht[char] += 1
-        
-        res = ""
-        left, right = 0, 0
+        # we need the t is smaller
+        len_s = len(s)
+        len_t = len(t)
+        if len_t > len_s: return ""
+        # initialize the two dicts to record the two
+        dict_s, dict_t = defaultdict(int), defaultdict(int)
+        # record the t
+        for i in t:
+            dict_t[i] += 1
+        # record the minimum string
+        min_string = ""
+        # how many char satisfy already
         cnt = 0
-        n = len(s)
-
-        while right < n:
-            hs[s[right]] += 1
-            if hs[s[right]] <= ht[s[right]]:
+        
+        # do the loop in s
+        # use two pointers left and right
+        left, right = 0, 0
+        while right < len_s:
+            # add the number
+            dict_s[s[right]] += 1
+            # judge the right place
+            if dict_s[s[right]] <= dict_t[s[right]]:
+                # means that can not satisfy
                 cnt += 1
-            
-            while left <= right and hs[s[left]] > ht[s[left]]:
-                hs[s[left]] -= 1
+            # if the number can satisfy on the left
+            while left <= right and dict_s[s[left]] > dict_t[s[left]]:
+                dict_s[s[left]] -= 1
                 left += 1
-
+            # if already satisfy
             if cnt == len(t):
-                if not res or right - left + 1 < len(res):
-                    res = s[left:right+1]
-
+                if not min_string or right - left + 1 < len(min_string):
+                    min_string = s[left:right+1]
             right += 1
 
-        return res
+        return min_string
